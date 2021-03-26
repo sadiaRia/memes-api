fa033f17addcff8a16907d6b9c1f4f509acace66
sadia <sadia@techhivebd.com>
2021-03-26 21:00:46 +0600

    add like count api & get conetect throug url

--- src/server/config/routes/content.js
+++ src/server/config/routes/content.js
@@ -3,6 +3,8 @@ const ContentController = require("../../content");
 module.exports = app => {
   app.post('/image', ContentController.uploadImage);
   app.post('/content/create', ContentController.create);  // after uploading the image user have to save it 
+  app.get('/content-link', ContentController.getImageByLink);  // after uploading the image user have to save it 
+  app.put('/content/add-like/:id', ContentController.addLike);  // after uploading the image user have to save it 
 
 
 
--- src/server/content/index.js
+++ src/server/content/index.js
@@ -5,7 +5,7 @@ const _ = require('lodash'),
 
 const uploadImage = (req, res) => {
   if (!config.s3ImageConfiguration.secretAccessKey) {
-    return res.status(201).send('for security purpose I can not provide all those credential if you give those credential properly code will work insha allah');
+    return res.status(202).send('for security purpose I can not provide all those credential if you give those credential properly code will work insha allah');
   }
   const singleUpload = uploader.upload.single('image');
   singleUpload(req, res, (err) => {
@@ -24,9 +24,47 @@ const create = async (req, res) => {
   return res.status(201).send(newContent);
 }
 
+// - People can view  by visiting this link.
+const getImageByLink = async (req, res) => {
+  let url = req.query.url;
+  if (!url) { return res.status(202).send('Invalid request'); }
+  const content = await Content.findOneAndUpdate({ url }, { $inc: { viewCount: 1 } }, { new: true }).catch((err) => {
+    return res.status(400).send('Failed to fetch content!');
+  })
+  return res.status(200).send(content);
+}
+
+// - People like the image by visiting this link.
+const addLike = (req, res) => {
+  const contentId = req.params.id;
+  const likedBy = req.body.likedBy; //userId of current user
+
+  Content.findById(contentId)
+    .populate({ path: 'user', model: 'User', select: ['name'] })
+    .exec((err, _content) => {
+      if (err) { return res.status(400).send(err); }
+      if (!_content) { return res.status(404).send('Content not fount'); }
+
+      if (_content.likedBy.includes(likedBy)) {
+        return res.status(200).send({ likesCount: _content.likesCount, likedBy: _content.likedBy })
+      }
+
+      _content.likesCount = _content.likesCount + 1;
+      _content.likedBy = _content.likedBy || [];
+      if (likedBy) {
+        _content.likedBy.push(likedBy);
+      }
+      _content.save((err) => {
+        if (err) { return res.status(400).send(err); }
+        return res.status(200).send({ likesCount: _content.likesCount, likedBy: _content.likedBy })
+      });
+    });
+}
 
 
 module.exports = {
   uploadImage,
-  create
+  create,
+  getImageByLink,
+  addLike
 }
